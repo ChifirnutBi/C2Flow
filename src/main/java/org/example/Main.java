59b891c388888c119d90c8923488f9b7ca2293ea
Denis <denisarzubov8@gmail.com>
2025-11-21 23:18:54 +0200

    GeminiExceptionHandler deleted

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,8 +11,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Thread.setDefaultUncaughtExceptionHandler(new GeminiExceptionHandler(150));
-
         FlowchartGenAI flowchartGenAI = FlowchartGenAI.getInstance();
         Config config = Config.getInstance();
 
